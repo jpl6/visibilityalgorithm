8d0c218198481ca148737488afda6d6520b929cd
Jaime Parreño Laporta <jaime.p.laporta@gmail.com>
2023-01-16 19:30:13 +0100

    add rest controller

---
+++ src/main/java/es/jaime/pruebatecnica/product/infrastructure/controller/ProductRest.java
@@ -0,0 +1,24 @@
+package es.jaime.pruebatecnica.product.infrastructure.controller;
+
+import es.jaime.pruebatecnica.product.application.ShowAvailable;
+import es.jaime.pruebatecnica.product.application.dto.ProductResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("product")
+public class ProductRest {
+    private final ShowAvailable showAvailable;
+
+    public ProductRest(ShowAvailable showAvailable) {
+        this.showAvailable = showAvailable;
+    }
+
+    @GetMapping("stock")
+    public ResponseEntity<ProductResponse> filterAvailableProducts() {
+        return new ResponseEntity<>(showAvailable.showAvailableProducts(), HttpStatus.OK);
+    }
+}
