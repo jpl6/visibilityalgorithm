8c47acfeee5289e5696f6253dd63cd5c095a804f
Jaime Parreño Laporta <jaime.p.laporta@gmail.com>
2023-01-17 12:53:19 +0100

    fix port test

--- src/test/java/es/jaime/pruebatecnica/shared/AbstractServiceTest.java
+++ src/test/java/es/jaime/pruebatecnica/shared/AbstractServiceTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
